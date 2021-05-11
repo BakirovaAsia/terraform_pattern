e2d6a29ee2914aa16a8831cdd0d6cf83b623a795
BakirovaAsia <bakirova.asia@gmail.com>
2021-05-11 19:30:25 +0300

    no output

--- pattern.tf
+++ pattern.tf
@@ -53,8 +53,3 @@ resource "yandex_vpc_subnet" "subnet-1" {
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-
-output "internal_ip_address_vm_1" {
-  for_each = yandex_compute_instance.vm
-  value = each.network_interface.0.nat_ip_address
-}
